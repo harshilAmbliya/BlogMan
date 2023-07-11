d6b0f9ae8f54010462a86dca7a1eb1d3e19e10f5
harshilAmbliya <harshilambliya15@gmail.com>
2023-07-11 15:28:04 +0530

    feat: env commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env:{
+        DATABASE_URL:"mongodb+srv://harshilambliya15:blogpasswordnotgive@cluster0.ywrw3gp.mongodb.net/blogman"
+    }
+}
 
 module.exports = nextConfig
